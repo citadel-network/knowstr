1d6e4473290e66dcc439f16a7420904a6036505f
donna <jgdv73@protonmail.com>
2024-08-28 09:43:04 +0200

    added test Add a new Relation Type

--- src/components/RelationTypesWrapper.test.ts
+++ src/components/RelationTypesWrapper.test.ts
@@ -72,3 +72,39 @@ test("Edit color of a Relation Type", async () => {
     })
   );
 });
+
+test("Add a new Relation Type", async () => {
+  const [alice] = setup([ALICE]);
+  const { relayPool } = renderApp({
+    ...alice(),
+    initialRoute: `/relationTypes`,
+  });
+  await screen.findByText("Edit Relation Types");
+  fireEvent.click(screen.getByLabelText("color of new relationType"));
+  fireEvent.click(await screen.findByTitle("#9c27b0"));
+  await userEvent.keyboard("new RelationType");
+  fireEvent.click(screen.getByLabelText("save new relationType"));
+  fireEvent.click(screen.getByText("Save"));
+
+  await waitFor(() =>
+    expect(
+      relayPool.getEvents().filter(filterRelationTypesEvents)
+    ).toHaveLength(1)
+  );
+  const event = relayPool.getEvents().filter(filterRelationTypesEvents)[0];
+  const eventContent = JSON.parse(event.content);
+  const relationTypeId =
+    Object.keys(eventContent).find((key) => key !== "") || "";
+  expect(event).toEqual(
+    expect.objectContaining({
+      kind: 11076,
+      pubkey:
+        "f0289b28573a7c9bb169f43102b26259b7a4b758aca66ea3ac8cd0fe516a3758",
+      tags: [],
+      content: JSON.stringify({
+        "": { c: "#027d86", l: "Default" },
+        [relationTypeId]: { c: "#9c27b0", l: "new RelationType" },
+      }),
+    })
+  );
+});
