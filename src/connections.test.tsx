8af3161bb4afaee64659eed1e1843d58c6ae016b
donna <jgdv73@protonmail.com>
2024-10-10 17:16:47 +0200

    add aggregateNegativeWeightedVotes and countRelevanceVoting

--- src/connections.test.tsx
+++ src/connections.test.tsx
@@ -8,6 +8,8 @@ import {
   shortID,
   countRelationVotes,
   aggregateWeightedVotes,
+  aggregateNegativeWeightedVotes,
+  countRelevanceVoting,
 } from "./connections";
 import { ALICE, BOB, CAROL } from "./utils.test";
 import { newRelations } from "./ViewContext";
@@ -131,10 +133,10 @@ test("count relation votes", () => {
     countRelationVotes(List([aliceVotes, bobVotes, carolVotes]), vote.id, "PRO")
   ).toEqual(
     Map({
-      [optionA.id]: 10454.545454545454, // 5/11+1/11+3/6 *10000 = 10454.545454545454
-      [optionB.id]: 8787.878787878788, // 3/11+3/11+2/6 *10000 = 8787.878787878788
-      [optionC.id]: 5303.030303030303, // 2/11+2/11+1/6 *10000 = 5303.030303030303
-      [optionD.id]: 5454.545454545454, // 5/11+1/11 * 10000 = 5454.545454545454
+      [optionA.id]: 10454.545454545454, // 5/11+1/11+3/6 *10000
+      [optionB.id]: 8787.878787878788, // 3/11+3/11+2/6 *10000
+      [optionC.id]: 5303.030303030303, // 2/11+2/11+1/6 *10000
+      [optionD.id]: 5454.545454545454, // 5/11+1/11 * 10000
     })
   );
 });
@@ -156,7 +158,157 @@ test("aggregate weighted votes", () => {
       A: 21.515151515151512, // 5/11*20 + 1/11*100 + 2/6*10
       B: 52.57575757575757, // 3/11*20 + 5/11*100 + 1/6*10
       C: 35.90909090909091, // 2/11*20 + 3/11*100 + 3/6*10
-      D: 21, //20+1
+      D: 21, // 20+1
+    })
+  );
+});
+
+test("aggregate negative weighted votes", () => {
+  const alice = ["A", "B", "C", "D"];
+  const bob = ["B", "C", "D", "A"];
+  const carol = ["C", "A", "B"];
+  const dan = ["D"];
+
+  const listsOfVotes = List([
+    { items: List(alice), weight: 20 },
+    { items: List(bob), weight: 100 },
+    { items: List(carol), weight: 10 },
+    { items: List(dan), weight: 1 },
+  ]);
+  expect(aggregateNegativeWeightedVotes(listsOfVotes)).toEqual(
+    Map({
+      A: -65,
+      B: -65,
+      C: -65,
+      D: -60.5,
+    })
+  );
+});
+
+test("count relation votes and also aggregate negative weights", () => {
+  const vote = newNode("VOTING", ALICE.publicKey);
+  const optionA = newNode("A", ALICE.publicKey);
+  const optionB = newNode("B", ALICE.publicKey);
+  const optionC = newNode("C", ALICE.publicKey);
+  const optionD = newNode("D", ALICE.publicKey);
+  const optionE = newNode("E", ALICE.publicKey);
+  const optionF = newNode("F", ALICE.publicKey);
+  const optionG = newNode("G", ALICE.publicKey);
+
+  // First Vote: multiple votes
+  const aliceVotes = bulkAddRelations(
+    newRelations(vote.id, "", ALICE.publicKey),
+    [optionA.id, optionB.id, optionC.id, optionD.id] // 5/11, 3/11, 2/11, 1/11 *10000
+  );
+  const bobVotes = bulkAddRelations(
+    newRelations(vote.id, "not_relevant", BOB.publicKey),
+    [optionD.id, optionB.id, optionC.id, optionA.id] // 1/2, 1/2, 1/2, 1/2 *10000
+  );
+  const carolVotes = bulkAddRelations(
+    newRelations(vote.id, "", CAROL.publicKey),
+    [optionA.id, optionB.id, optionC.id] // 3/6, 2/6, 1/6 *10000
+  );
+
+  expect(
+    countRelevanceVoting(List([aliceVotes, bobVotes, carolVotes]), vote.id)
+  ).toEqual(
+    Map({
+      [optionA.id]: 4545.454545454544, // 5/11-1/2+3/6 *10000
+      [optionB.id]: 1060.60606060606, // 3/11-1/2+2/6 *10000
+      [optionC.id]: -1515.151515151515, // 2/11-1/2+1/6 *10000
+      [optionD.id]: -4090.909090909091, // 1/11-1/2 * 10000
+    })
+  );
+
+  // Second Vote: positive votes add up
+  const secondVote = newNode("SECOND VOTING", ALICE.publicKey);
+  const secondAliceVotes = bulkAddRelations(
+    newRelations(secondVote.id, "", ALICE.publicKey),
+    [optionA.id, optionB.id, optionC.id, optionD.id, optionE.id] // 8/19, 5/19, 3/19, 2/19, 1/19 *10000
+  );
+  const secondBobVotes = bulkAddRelations(
+    newRelations(secondVote.id, "", BOB.publicKey),
+    [optionB.id, optionC.id, optionD.id] // 3/6, 2/6, 1/6 *10000
+  );
+  expect(
+    countRelevanceVoting(
+      List([secondAliceVotes, secondBobVotes]),
+      secondVote.id
+    )
+  ).toEqual(
+    Map({
+      [optionA.id]: 4210.526315789473, // 8/19 *10000
+      [optionB.id]: 7631.578947368421, // 5/19+3/6 *10000
+      [optionC.id]: 4912.2807017543855, // 3/19+2/6 *10000
+      [optionD.id]: 2719.298245614035, // 2/19+1/6 *10000
+      [optionE.id]: 526.3157894736842, // 1/19 *10000
+    })
+  );
+
+  // Third Vote: negative votes always count -1/2
+  const thirdVote = newNode("THIRD VOTING", ALICE.publicKey);
+  const thirdAliceVotes = bulkAddRelations(
+    newRelations(thirdVote.id, "not_relevant", ALICE.publicKey),
+    [optionA.id, optionB.id, optionC.id]
+  );
+
+  expect(countRelevanceVoting(List([thirdAliceVotes]), thirdVote.id)).toEqual(
+    Map({
+      [optionA.id]: -5000, // -1/2 *10000
+      [optionB.id]: -5000, // -1/2 *10000
+      [optionC.id]: -5000, // -1/2 *10000
+    })
+  );
+
+  // Fourth Vote: positive votes are fibonacci-weighted sequence
+  const fourthVote = newNode("FOURTH VOTING", ALICE.publicKey);
+  const fourthAliceVotes = bulkAddRelations(
+    newRelations(fourthVote.id, "", ALICE.publicKey),
+    [
+      optionA.id,
+      optionB.id,
+      optionC.id,
+      optionD.id,
+      optionE.id,
+      optionF.id,
+      optionG.id,
+    ] // 21/53, 13/53, 8/53, 5/53, 3/53, 2/53, 1/53 *10000
+  );
+
+  expect(countRelevanceVoting(List([fourthAliceVotes]), fourthVote.id)).toEqual(
+    Map({
+      [optionA.id]: 3962.2641509433965, // 21/53 *10000
+      [optionB.id]: 2452.830188679245, // 13/53 *10000
+      [optionC.id]: 1509.433962264151, // 8/53 *10000
+      [optionD.id]: 943.3962264150944, // 5/53 *10000
+      [optionE.id]: 566.0377358490566, // 3/53 *10000
+      [optionF.id]: 377.35849056603774, // 2/53 *10000
+      [optionG.id]: 188.67924528301887, // 1/53 *10000
+    })
+  );
+
+  // Fifth Vote: positive and negative votes cancel out
+  const fifthVote = newNode("FIFTH VOTING", ALICE.publicKey);
+  const fifthAliceVotes = bulkAddRelations(
+    newRelations(fifthVote.id, "", ALICE.publicKey),
+    [optionA.id] // 1
+  );
+  const fifthBobVotes = bulkAddRelations(
+    newRelations(fifthVote.id, "not_relevant", BOB.publicKey),
+    [optionA.id] // -1/2,
+  );
+  const fifthCarolVotes = bulkAddRelations(
+    newRelations(fifthVote.id, "not_relevant", CAROL.publicKey),
+    [optionA.id] // -1/2,
+  );
+  expect(
+    countRelevanceVoting(
+      List([fifthAliceVotes, fifthBobVotes, fifthCarolVotes]),
+      fifthVote.id
+    )
+  ).toEqual(
+    Map({
+      [optionA.id]: 0, // 1-1/2-1/2
     })
   );
 });
